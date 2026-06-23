b30f41a06ca6e94fd4f34d58aa4620f29f2cab86
Aliyah Nur Dafika <aliyahnurdafika@gmail.com>
2026-06-23 08:37:44 -0700

    Update file content extraction by implementing SonarQube Workflow

--- src/extractors/extract_file_content.py
+++ src/extractors/extract_file_content.py
@@ -1,34 +1,35 @@
-
-from src.file_filter import should_include_file
-
-
 # Create function to extract files
-def extract_files(repo_data: dict) -> list[dict]:
-    # 1. Extract default branch name
+def extract_files(repo_data: dict, sonar_data: dict) -> list[dict]:
+    # 1. Build set of file paths from sonar components (this is the filter)
+    sonar_paths = set()
+    for comp in sonar_data["components"]:
+        sonar_paths.add(comp["path"])
+
+    # 2. Extract default branch name
     default_branch_name = repo_data["metadata"]["default_branch"]
 
-    #2. Last commit on default branch
+    # 3. Last commit on default branch
     latest_sha = None
     for branch in repo_data["branches"]:
         if branch["name"] == default_branch_name:
             latest_sha = branch["commit"]["sha"]
             break   # Exit loop once found
-  
 
-    # 3. Snapshot of the commit
+
+    # 4. Snapshot of the commit
     latest_snapshot = None
     for snapshot in repo_data["snapshots"]:
         if snapshot["commit_sha"] == latest_sha:
             latest_snapshot = snapshot
             break   # Exit loop once found
-    
 
-    # 4 Extract file content
+
+    # 5. Extract file content only for files that exist in sonar list
     file_version = repo_data["file_versions"]
     result = []                                 # list for the results
     for file in latest_snapshot["files"]:
 
-        if should_include_file(file["file_path"]):
+        if file["file_path"] in sonar_paths:
             version_id = file["file_version_id"]
 
             result.append({     # To store data in a list
--- src/run_extract_file_content.py
+++ src/run_extract_file_content.py
@@ -3,40 +3,67 @@ from src.extractors.extract_file_content import extract_files
 from datetime import datetime
 from pathlib import Path
 
-json_path = ("/Users/aliyahnurdafika/Documents/preandpost/data/UBCO-COSC499-Winter-2018-Term-1-2/project-1-crm-for-non-profits-trellis-crm.json")
+json_path = ("/Users/aliyahnurdafika/Developer/preandpost/data/UBCO-COSC499-Winter-2018-Term-1-2/project-1-crm-for-non-profits-trellis-crm.json")
+sonar_path =("/Users/aliyahnurdafika/Developer/preandpost/data/UBCO-COSC499-Winter-2018-Term-1-2/sonar_qube/files_page_1.json")
 
 with open(json_path, "r", encoding="utf-8") as f:
     repo_data = json.load(f)
 
-    files = extract_files(repo_data)
+with open(sonar_path, "r", encoding="utf-8") as f:
+    sonar_data = json.load(f)
 
-    output = {
-        "owner": repo_data["owner"],
-        "repository": repo_data["repository"],
-        "default_branch": repo_data["metadata"]["default_branch"],
-        "file_count": len(files),
-        "files": files,
-        "extracted_at": datetime.now().isoformat()
-    }
+files = extract_files(repo_data, sonar_data)
 
-    output_path = (Path("data")/output["owner"]/"file_content"/f"{output['repository']}.json")
+output = {
+    "owner": repo_data["owner"],
+    "repository": repo_data["repository"],
+    "default_branch": repo_data["metadata"]["default_branch"],
+    "file_count": len(files),
+    "files": files,
+    "extracted_at": datetime.now().isoformat()
+}
 
-    with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(output, f, indent = 2)
+output_path = (
+    Path(
+        "/Users/aliyahnurdafika/Library/CloudStorage/"
+        "OneDrive-UBC/File Hui, Bowen - repo data"
+    )
+    / output["owner"]
+    / "file_content"
+    / f"{output['repository']}.json"
+)
 
-    file_list_output = {
-        "owner": output["owner"],
-        "repository": output["repository"],
-        "default_branch": output["default_branch"],
-        "file_count": output["file_count"],
-        "filenames": [Path(item["path"]).name for item in files],
-        "extracted_at": output["extracted_at"],
-    }
+output_path.parent.mkdir(parents=True, exist_ok=True)
 
-    file_list_path = (Path("data")/output["owner"]/"file_list"/f"{output['repository']}.json")
-    file_list_path.parent.mkdir(parents=True, exist_ok=True)
 
-    with open(file_list_path, "w", encoding="utf-8") as f:
-        json.dump(file_list_output, f, indent=2)
+
+with open(output_path, "w", encoding="utf-8") as f:
+    json.dump(output, f, indent = 2)
+
+file_list_output = {
+    "owner": output["owner"],
+    "repository": output["repository"],
+    "default_branch": output["default_branch"],
+    "file_count": output["file_count"],
+    "filenames": [Path(item["path"]).name for item in files],
+    "extracted_at": output["extracted_at"],
+}
+
+
+file_list_path = (
+    Path(
+        "/Users/aliyahnurdafika/Library/CloudStorage/"
+        "OneDrive-UBC/File Hui, Bowen - repo data"
+    )
+    / output["owner"]
+    / "file_list"
+    / f"{output['repository']}.json"
+)
+
+ 
+file_list_path.parent.mkdir(parents=True, exist_ok=True)
+
+with open(file_list_path, "w", encoding="utf-8") as f:
+    json.dump(file_list_output, f, indent=2)
 
 # Run: python -m src.run_extract_file_content
